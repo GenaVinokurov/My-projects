89ca01685bcd82afc4b73ea2418e09777870312b
GenaVinokurov <genabrest97@gmail.com>
2021-04-28 13:28:39 +0300

    feat: add functions and fix popup

--- online-zoo/main.js
+++ online-zoo/main.js
@@ -1,23 +1,45 @@
 // popup
 const btnDonate = document.getElementById("btn-donate");
 const btnFeedback = document.getElementById("btn-feedback")
+const popupsWrapper = document.querySelector(".popups-bg")
 let donate = document.getElementById("donate")
 let feedback = document.getElementById("feedback")
 let close = document.querySelectorAll(".popup-close")
 
 btnDonate.addEventListener("click", function (e) {
   donate.classList.add("active")
+  popupsWrapper.classList.add("active")
 })
 btnFeedback.addEventListener("click", function (e) {
   feedback.classList.add("active")
+  popupsWrapper.classList.add("active")
 })
 close.forEach(e => {
   e.addEventListener("click", function (event) {
     event.preventDefault()
-    donate.classList.remove('active') || feedback.classList.remove('active')
+    popupsWrapper.classList.remove("active")
+    donate.classList.remove('active')
+    feedback.classList.remove('active')
+    popupCard.classList.remove('active')
   })
 });
+popupsWrapper.addEventListener('click', function (e) {
+  popupsWrapper.classList.remove("active")
+  donate.classList.remove('active')
+  feedback.classList.remove('active')
+})
 
+const btnDonateNext = document.querySelector(".btn-popup__donate")
+const popupCard = document.querySelector(".popup-card")
+const btnCard = document.querySelector(".btn-card")
+btnDonateNext.addEventListener("click", function (e) {
+  popupCard.classList.add('active');
+  donate.classList.remove('active');
+})
+btnCard.addEventListener("click", function (e) {
+  popupCard.classList.remove('active')
+  popupsWrapper.classList.remove("active")
+})
 //---------------------burger-menu----------------------------
 
 const burgerOpen = document.querySelector(".burger-menu__button");
@@ -30,3 +52,4 @@ burgerOpen.addEventListener('click', function (e) {
 burgerClose.addEventListener('click', function (e) {
   burgerMenu.classList.remove('js-act')
 })
+
