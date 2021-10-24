f2ce424f91be664f4cc5c8d69708e6968f27f7fd
GenaVinokurov <genabrest97@gmail.com>
2021-10-24 23:10:51 +0300

    feat: add audio player and language switch

--- momentum/src/index.js
+++ momentum/src/index.js
@@ -1,4 +1,41 @@
 const body = document.querySelector('body');
+// --------------------------------------Language
+
+const langArr = {
+  "greetingLang": {
+    en: "Good",
+    ru: "Добрый",
+  },
+  "wind": {
+    en: "wind speed: ",
+    ru: "скорость ветра: ",
+  },
+  "humidity": {
+    en: "humidity: ",
+    ru: "влажность: ",
+  },
+}
+
+
+const select = document.querySelector('.select-language');
+
+select.addEventListener('change', changeURLLanguage);
+
+function changeURLLanguage() {
+  let lang = select.value;
+  location.href = window.location.pathname + "#" + lang;
+  location.reload();
+}
+
+let hash;
+function changeLanguage() {
+  hash = window.location.hash;
+  hash = hash.substr(1);
+  select.value = hash;
+
+  return hash
+}
+changeLanguage()
 
 //------------------------------------------TIME
 const timeHtml = document.querySelector('.time');
@@ -25,6 +62,38 @@ const getTimeOfDate = function () {
   const date = new Date();
   const hours = date.getHours();
   const greetingHtml = document.querySelector('.greeting');
+  if (hash == 'en') {
+    if (hours < 6) {
+      greeting = 'night';
+    } else {
+      if (hours < 12) {
+        greeting = 'morning';
+      } else {
+        if (hours < 18) {
+          greeting = 'afternoon';
+        } else {
+          greeting = 'evening';
+        }
+      }
+    }
+  } else {
+    if (hours < 6) {
+      greeting = 'ночи';
+    } else {
+      if (hours < 12) {
+        greeting = 'утро';
+      } else {
+        if (hours < 18) {
+          greeting = 'день';
+        } else {
+          greeting = 'вечер';
+        }
+      }
+    }
+  }
+
+  console.log()
+  greetingHtml.textContent = `${langArr['greetingLang'][hash]} ${greeting}`;
   if (hours < 6) {
     greeting = 'night';
   } else {
@@ -38,7 +107,6 @@ const getTimeOfDate = function () {
       }
     }
   }
-  greetingHtml.textContent = `Good ${greeting}`;
   return greeting;
 }
 getTimeOfDate();
@@ -96,7 +164,7 @@ const wind = document.querySelector('.wind');
 const humidity = document.querySelector('.humidity');
 
 async function getWeather() {
-  const url = `https://api.openweathermap.org/data/2.5/weather?q=${city.value}&lang=en&appid=94e79d036c9efc29c9a2c590a84d4fdc&units=metric`;
+  const url = `https://api.openweathermap.org/data/2.5/weather?q=${city.value}&lang=${hash}&appid=94e79d036c9efc29c9a2c590a84d4fdc&units=metric`;
   const res = await fetch(url);
   const data = await res.json();
 
@@ -105,8 +173,8 @@ async function getWeather() {
     weatherIcon.classList.add(`owf-${data.weather[0].id}`);
     temperature.textContent = `${Math.ceil(data.main.temp)}°C`;
     weatherDescription.textContent = data.weather[0].description;
-    wind.textContent = `Wind speed: ${Math.ceil(data.wind.speed)} m/c`
-    humidity.textContent = `Humidity: ${Math.ceil(data.main.humidity)} %`
+    wind.textContent = `${langArr['wind'][hash]} ${Math.ceil(data.wind.speed)} m/c`
+    humidity.textContent = `${langArr['humidity'][hash]} ${Math.ceil(data.main.humidity)} %`
   }
   catch (err) {
     if (city.value == '') {
@@ -151,7 +219,7 @@ const quote = document.querySelector('.quote');
 const author = document.querySelector('.author');
 const quoteButton = document.querySelector('.change-quote');
 async function getQuotes() {
-  const quotes = 'data.json';
+  const quotes = `data-${hash}.json`;
   const res = await fetch(quotes);
   const data = await res.json();
   const properties = Object.getOwnPropertyNames(data);
@@ -171,25 +239,52 @@ quoteButton.addEventListener('click', getQuotes);
 //----------------------------------Audio player
 import playList from '../src/playList.js';
 
+const player = document.querySelector('.player');
+const musicTitle = document.querySelector('.music-title');
+const playerRewind = document.querySelector('.player__rewind');
+const playerVolume = document.querySelector('.player__volume');
+const volumeButton = document.querySelector('.player__volume-btn');
+
 const play = document.querySelector('.play');
 let isPlay = false;
 const audio = new Audio();
 let playNum = 0;
 function playAudio() {
   audio.src = playList[playNum].src;
+  const value = 0
   audio.currentTime = 0;
   if (isPlay === false) {
     audio.pause();
     audio.play();
     isPlay = true;
     play.classList.add('pause');
+    playerRewind.style.background = `linear-gradient(to right, #710707 ${value * 100}%, #710707 ${value}%, #C4C4C4 ${value}%, #C4C4C4 100%)`;
+
   } else {
     audio.pause()
     isPlay = false;
     play.classList.remove('pause');
+    playerRewind.style.background = `linear-gradient(to right, #710707 ${value * 100}%, #710707 ${value}%, #C4C4C4 ${value}%, #C4C4C4 100%)`;
+
   }
 }
 play.addEventListener('click', playAudio);
+
+audio.addEventListener(
+  "loadeddata",
+  () => {
+    console.log(audio.duration)
+    player.querySelector(".player-time .length").textContent = getTimeCodeFromNum(audio.duration);
+    audio.volume = .5;
+  },
+  false
+);
+
+setInterval(() => {
+  player.querySelector(".player-time .current").textContent = getTimeCodeFromNum(
+    audio.currentTime
+  );
+}, 500);
 
 const playNextBtn = document.querySelector('.play-next');
 const playPrevBtn = document.querySelector('.play-prev');
@@ -230,7 +325,72 @@ for (let i = 0; i < playList.length; i++) {
 const liCollection = document.querySelectorAll('.play-item');
 liCollection[playNum].classList.add('item-active')
 
-document.addEventListener('click', function (e) {
+musicTitle.textContent = playList[playNum].title;
+player.addEventListener('click', function () {
+  musicTitle.textContent = playList[playNum].title;
   liCollection.forEach(i => i.classList.remove('item-active'));
   liCollection[playNum].classList.add('item-active');
 })
+
+
+function rewind() {
+  const value = this.value;
+  this.style.background = `linear-gradient(to right, #710707 ${value * 100}%, #710707 ${value}%, #C4C4C4 ${value}%, #C4C4C4 100%)`;
+}
+
+playerVolume.addEventListener('input', function () {
+  let v = this.value;
+  audio.volume = v;
+  if (audio.volume != 0) {
+    volumeButton.classList.remove('mute');
+  } else {
+    volumeButton.classList.add('mute');
+  }
+})
+
+playerRewind.addEventListener('input', rewind);
+playerVolume.addEventListener('input', rewind);
+
+function volumeFunc() {
+  if (audio.volume != 0) {
+    audio.volume = 0;
+    volumeButton.classList.add('mute');
+    playerVolume.value = 0
+    playerVolume.style.background = `linear-gradient(to right, #710707 ${0 * 100}%, #710707 ${0}%, #C4C4C4 ${0}%, #C4C4C4 100%)`;
+  } else {
+    audio.volume = 0.5;
+    volumeButton.classList.remove('mute');
+    playerVolume.value = 0.5;
+    playerVolume.style.background = `linear-gradient(to right, #710707 ${0.5 * 100}%, #710707 ${0.5}%, #C4C4C4 ${0.5}%, #C4C4C4 100%)`;
+  }
+}
+volumeButton.addEventListener('click', volumeFunc);
+
+audio.ontimeupdate = function () {
+  let d = audio.duration;
+  let c = audio.currentTime;
+  playerRewind.value = c / d;
+  const value = playerRewind.value;
+  playerRewind.style.background = `linear-gradient(to right, #710707 ${value * 100}%, #710707 ${value}%, #C4C4C4 ${value}%, #C4C4C4 100%)`;
+}
+playerRewind.addEventListener('click', function () {
+  let w = this.offsetWidth;
+  let o = event.offsetX;
+  audio.pause();
+  audio.currentTime = audio.duration * (o / w);
+  audio.play();
+  play.classList.add('pause');
+});
+
+function getTimeCodeFromNum(num) {
+  let seconds = parseInt(num);
+  let minutes = parseInt(seconds / 60);
+  seconds -= minutes * 60;
+  const hours = parseInt(minutes / 60);
+  minutes -= hours * 60;
+
+  if (hours === 0) return `${minutes}:${String(seconds % 60).padStart(2, 0)}`;
+  return `${String(hours).padStart(2, 0)}:${minutes}:${String(
+    seconds % 60
+  ).padStart(2, 0)}`;
+}
