01f3bb8860d49f5e880781c7f99cff58cf97e39d
GenaVinokurov <genabrest97@gmail.com>
2021-12-06 00:35:19 +0300

    fix: sources bug(didn't event for id)

--- news-JS-main/src/components/view/sources/sources.ts
+++ news-JS-main/src/components/view/sources/sources.ts
@@ -4,33 +4,21 @@ export interface ISource {
   id: string;
   name: string;
 }
-
 class Sources {
   draw(data: ISource[]) {
     const fragment = document.createDocumentFragment() as DocumentFragment;
     const sourceItemTemp = document.querySelector('#sourceItemTemp') as HTMLTemplateElement;
-    const allSources = document.querySelector('.sources');
 
     data.forEach((item) => {
       const sourceClone = sourceItemTemp.content.cloneNode(true) as HTMLTemplateElement;
 
-      const itemName = sourceClone.querySelector('.source__item-name');
-
-      if (itemName === null || sourceClone === null || allSources === null) {
-        return;
-      }
-      itemName.textContent = item.name;
-      sourceItemTemp.setAttribute('data-source-id', item.id);
+      (<HTMLElement>sourceClone.querySelector('.source__item-name')).textContent = item.name;
+      (<HTMLElement>sourceClone.querySelector('.source__item')).setAttribute('data-source-id', item.id);
 
       fragment.append(sourceClone);
     });
 
-
-
-    if (allSources === null) {
-      return;
-    }
-    allSources.append(fragment);
+    (<HTMLElement>document.querySelector('.sources')).append(fragment);
   }
 }
 
