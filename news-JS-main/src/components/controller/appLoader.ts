d4f91cadf5ee43e03d449a07f978c249d66c2d94
GenaVinokurov <genabrest97@gmail.com>
2021-12-06 15:58:58 +0300

    fix: replace ulr for deploy

--- news-JS-main/src/components/controller/appLoader.ts
+++ news-JS-main/src/components/controller/appLoader.ts
@@ -2,7 +2,7 @@ import Loader from './loader';
 
 class AppLoader extends Loader {
   constructor() {
-    super('https://newsapi.org/v2/', {
+    super('https://nodenews.herokuapp.com/', {
       apiKey: '0a50c7d3f7924068b50c55e073adff3e', // получите свой ключ https://newsapi.org/
     });
   }
