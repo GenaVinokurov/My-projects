812f03bdec6f7f25078ddbb6da03817e0f9504fd
GenaVinokurov <genabrest97@gmail.com>
2021-12-27 00:07:03 +0300

    add: garland

--- christmas-task/src/pages/tree/tree.ts
+++ christmas-task/src/pages/tree/tree.ts
@@ -8,6 +8,13 @@ class Tree extends RenderFavorites {
   audioControl: HTMLElement;
   playAudio: boolean;
   music: HTMLAudioElement;
+  garlandBtn: HTMLElement;
+  garlandContainer: HTMLElement;
+  multiBtn: HTMLElement;
+  greenBtn: HTMLElement;
+  blueBtn: HTMLElement;
+  redBtn: HTMLElement;
+  yellowBtn: HTMLElement;
   constructor() {
     super();
     this.audioControl = document.querySelector('.audio-control') as HTMLElement;
@@ -15,7 +22,19 @@ class Tree extends RenderFavorites {
     this.music.src = './assets/audio/audio.mp3';
     this.playAudio = false as boolean;
     this.audioControl.addEventListener('click', this.audio.bind(this));
-
+    this.garlandBtn = document.querySelector('.btn-garland') as HTMLElement;
+    this.garlandContainer = document.querySelector('.garland-tree-container') as HTMLElement;
+    this.multiBtn = document.querySelector('.multicolor-btn') as HTMLElement;
+    this.greenBtn = document.querySelector('.green-btn') as HTMLElement;
+    this.blueBtn = document.querySelector('.blue-btn') as HTMLElement;
+    this.redBtn = document.querySelector('.red-btn') as HTMLElement;
+    this.yellowBtn = document.querySelector('.yellow-btn') as HTMLElement;
+    this.multiBtn.addEventListener('click', this.changeColors.bind(this));
+    this.greenBtn.addEventListener('click', this.changeColors.bind(this));
+    this.blueBtn.addEventListener('click', this.changeColors.bind(this));
+    this.redBtn.addEventListener('click', this.changeColors.bind(this));
+    this.yellowBtn.addEventListener('click', this.changeColors.bind(this));
+    this.garlandBtn.addEventListener('click', this.garlandSwitch.bind(this));
   }
 
   audio() {
@@ -23,11 +42,41 @@ class Tree extends RenderFavorites {
       console.log(this.playAudio);
       this.music.play();
       this.playAudio = true;
+      this.audioControl.classList.add('active');
     } else if (this.playAudio === true) {
       this.music.pause();
       console.log(this.playAudio);
       this.playAudio = false;
+      this.audioControl.classList.remove('active');
     }
+  }
+
+  garlandSwitch() {
+    this.garlandContainer.classList.toggle('hide');
+    this.garlandBtn.classList.toggle('active');
+    if (this.garlandBtn.classList.contains('active')) {
+      this.garlandBtn.innerHTML = 'Выкл';
+    } else {
+      this.garlandBtn.innerHTML = 'Вкл';
+    }
+  }
+
+  changeColors(event: Event) {
+    const current = event.target as HTMLElement;
+    const color = current.dataset.color;
+    const lightropeCollection = document.querySelectorAll('.lightrope') as NodeList;
+    lightropeCollection.forEach(item => {
+      const list = item as HTMLElement;
+      const listCollection = list.querySelectorAll('li');
+      listCollection.forEach(elem => {
+        elem.classList.remove('multicolor');
+        elem.classList.remove('red');
+        elem.classList.remove('green');
+        elem.classList.remove('blue');
+        elem.classList.remove('yellow');
+        elem.classList.add(`${color}`);
+      });
+    });
   }
 
 }
